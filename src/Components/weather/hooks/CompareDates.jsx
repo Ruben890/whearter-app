c9981d82313280228f9c01cabe153fad753b255c
Darlin Ruben Nina C <78616515+Ruben890@users.noreply.github.com>
2023-06-02 14:40:36 -0400

    Update CompareDates.jsx

--- src/Components/weather/hooks/CompareDates.jsx
+++ src/Components/weather/hooks/CompareDates.jsx
@@ -1,12 +1,14 @@
 const CompareDates = (dateString) => {
     const currentDate = new Date();
     const targetDate = new Date(dateString);
-
+  
     return (
-        currentDate.getFullYear() === targetDate.getFullYear() &&
-        currentDate.getMonth() === targetDate.getMonth() &&
-        currentDate.getDate() === targetDate.getDate()
+      currentDate.getFullYear() === targetDate.getFullYear() &&
+      currentDate.getMonth() === targetDate.getMonth() &&
+      currentDate.getDate() === targetDate.getDate() &&
+      currentDate.getTime() === targetDate.getTime()
     );
-}
+  }
+  
 
 export default CompareDates
