bd873e725ebaa30e9ad2b3d6db561b8200fd0c1a
Darlin Ruben Nina C <78616515+Ruben890@users.noreply.github.com>
2023-06-05 15:55:01 -0400

    Update WeatherIcon.jsx

--- src/Components/weather/WeatherIcon.jsx
+++ src/Components/weather/WeatherIcon.jsx
@@ -4,10 +4,10 @@ const WeatherIcon = ({ description }) => {
     const renderIcon = (description) => {
         if (description in Icons) {
             const { icon } = Icons[description];
-            return <img className="weather-icon" src={icon} alt={description} width='150px'  height='150px'/>;
+            return <img className="weather-icon__imagen" src={icon} alt={description} width='150px'  height='150px'/>;
         } else {
             const { icon } = Icons.defaultIcon;
-            return <img className="weather-icon" src={icon} alt={description} width='150px'  height='150px'/>;
+            return <img className="weather-icon__imagen" src={icon} alt={description} width='150px'  height='150px'/>;
         }
     };
 
